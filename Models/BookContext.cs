fda0da64de23df745b4c68c9439fb536cf56cccf
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-23 12:49:33 +0530

    Add project files.

---
+++ Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Models
+{
+    public class BookContext: DbContext
+    {
+
+        public BookContext(DbContextOptions<BookContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<BookItem> BookItems { get; set; } = null!;
+    }
+}
---
+++ Models/BookItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStoreApi.Models
+{
+    public class BookItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genere { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
